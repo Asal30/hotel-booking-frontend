fbabb09fdbae44ac711bcd1cfccb11b54bdc9354
Asal Samarasinghe <asalsamarasinghe30@gmail.com>
2025-01-14 09:40:55 +0530

    Make basic routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./components/pages/admin-page/admin";
 import HomePage from "./components/pages/home-page/homePage";
+
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/*" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/admin-page/admin.jsx
@@ -0,0 +1,7 @@
+export default function Admin() {
+    return (
+        <div className="w-full h-[100vh] bg-red-400">
+            <h1>Admin</h1>
+        </div>
+    );
+}
